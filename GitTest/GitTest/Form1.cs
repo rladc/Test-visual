1bd6be67a3332af27a87f516e7560759436e0c5f
rladc <rladc@naver.com>
2017-05-18 16:07:50 +0900

    텍스트박스추가

--- GitTest/GitTest/Form1.cs
+++ GitTest/GitTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GitTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "테스트";
+        }
     }
 }
